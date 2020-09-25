710a25a83b67641773c5a2a4ac850ea98e179f54
MKalashnikau <mikita@fortvision.com>
2020-09-26 00:59:16 +0300

    new loader

--- src/App.js
+++ src/App.js
@@ -8,12 +8,49 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [activePage, setActivePage] = useState(1);
   const [totalResults, setTotalResults] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+
+  const style = {
+    navBar: {
+      position: 'absolute',
+      width: '100%',
+      top: '0',
+      right: '0',
+      display: 'flex',
+      justifyContent: 'center',
+
+    },
+    nav: {
+      display: 'block'
+    },
+    navbarCollapse: {
+      flexGrow: 'unset'
+    },
+    container: {
+      paddingTop: '100px',
+      textAlign: 'center'
+    },
+    input: {
+      width: '20rem'
+    },
+    button: {
+      width: '5rem'
+    },
+    a: {
+      color: '#fbdeaa'
+    },
+    div: {
+      border: 'none'
+    }
+  };
 
 
   async function fetchData(searchQuery, page) {
+    setLoading(true);
     const url = `https://www.omdbapi.com/?i=tt3896198&apikey=8523cbb8&s=${searchQuery}&page=${page}`;
     const res = await fetch(url);
     res.json().then((res) => {
+      setLoading(false);
       setTotalResults(res.totalResults);
       setCardsToShow(res.Search ? res.Search.slice(0, 8) : null);
     });
@@ -55,44 +92,63 @@ function App() {
 
 
   return (
-    <div className="container">
-      <div className="navbar navbar-expand-lg bg-light">
-        <div className="navbar-brand">Movie Catalog</div>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <form className="form-inline my-2 my-lg-0" onSubmit={submitHandler}>
-              <input
-                className="form-control mx-auto"
-                style={{width: "40rem"}}
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                value={searchQuery}
-                onChange={searchHandler}
-              />
-              <button
-                className="btn btn-outline-success mx-3 "
-                type="submit"
-                style={{width: "7rem"}}
-              >
-                Search
-              </button>
-            </form>
-            <li className="nav-item ">
-              <div className="nav-link dropdown-toggle mx- 3">Pavel Horski</div>
+    <div className="container" style={style.container}>
+      <div className="d-flex navbar navbar-expand-lg bg-light" style={style.navBar}>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" style={style.nav}>
+          <a className="navbar-brand" href="https://www.linkedin.com/company/web-friedns/">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C4D0BAQF_BaxcSbfO9Q/company-logo_200_200/0?e=2159024400&v=beta&t=B1bJCP1NGx-jRxHJUNwj5vWRHvvHUW3O9DoyY9c1xIA"
+              width="60" height="60"
+              className="d-inline-block align-content-center mr-3" alt="" loading="lazy"/>
+            Movie Catalog
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+          </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent"  style={style.navbarCollapse}>
+          <ul className="navbar-nav">
+            <li className='nav-item active'>
+              <form className="form-inline" onSubmit={submitHandler}>
+                <input
+                  className="form-control mx-auto"
+                  style={style.input}
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={searchQuery}
+                  onChange={searchHandler}
+                />
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0"
+                  type="submit"
+                  style={style.button}
+                >
+                  Search
+                </button>
+              </form>
+              <div className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle bg-info border-dark rounded" style={style.a}
+                   href="https://resume.io/r/87Qp7zhpt" data-toggle="dropdown">
+                  Pavel Horski
+                </a>
+              </div>
             </li>
           </ul>
         </div>
+        </nav>
       </div>
-      {cardsToShow && cardsToShow.length ? search() : null }
-      <div className="row" style={{border: "none"}}>
+      {cardsToShow && cardsToShow.length ? search() : <h1 className='m-3'>You searched for: <span
+        className="badge badge-secondary">{totalResults}</span> results found</h1>}
+      <div className="row" style={style.div}>
         {cardsToShow && cardsToShow.length ? (
           cardsToShow.map((card, id) => <Card card={card} key={id}/>)
-        ) : (
-          <div className='col-10 d-flex justify-content-center align-items-center'>
-            <Loading type='spin' color='#A8BDAB'/>
-          </div>
-        )}
+        ) : null
+        }
+        {loading ? (<div className='col-10 d-flex justify-content-center align-items-center'>
+          <Loading type='spin' color='#A8BDAB'/>
+        </div>) : null}
       </div>
       <div className='m-3'>
         <Pagination activePage={activePage}
--- src/Component/Card.js
+++ src/Component/Card.js
@@ -3,16 +3,18 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className='col-3 '>
+
+    <div className='col-xl-3 col-md-4 col-sm-6 col-12'>
       <img
         srcSet={props.card.Poster}
-        alt="..." className="img-thumbnail m-3 mh-100" style={{height: '370px'}}/>
-      <div className='ml-3 ml-0'>
+        alt="..." className="img-thumbnail m-3 mh-100 mw-100" style={{height: '367px'}} />
+      <div className='col'>
         <div>Name: {props.card.Title}</div>
         <div>Year: {props.card.Year}</div>
         <div>imdbID: {props.card.imdbID}</div>
         <div>Type: {props.card.Type}</div>
       </div>
     </div>
+
   );
 }
